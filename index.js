9d8caf0fa8f341a3f70ff3279271e5e51db18299
Michael Magsuci <michael@bookwana.com>
2020-04-24 17:33:55 +0800

    TRYB-9012 update script

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var wkhtmltopdf = require('./src/helpers/wkhtmltopdf');
-var MemoryStream = require('memorystream');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-exports.handler = function(event, context) {
-    var memStream = new MemoryStream();
-    var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
-    wkhtmltopdf(html_utf8, event.options, function(code, signal) { context.done(null, { pdf_base64: memStream.read().toString('base64') }); }).pipe(memStream);
-};
+exports.handler = async (event) => {
+    const html = '<h1>Test</h1>';
+    wkhtmltopdf(html, { pageSize: 'letter'}, function(error, stream) {
+        console.log("error:", error);
+        console.log("Here is the Stream", stream);
+    })
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+}; 
