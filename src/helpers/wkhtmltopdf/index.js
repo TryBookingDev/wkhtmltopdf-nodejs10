15c682a290d742e8a7e736d84c9e6f79b51ead1f
Michael Magsuci <michael@bookwana.com>
2020-04-24 21:34:39 +0800

    update script

--- src/helpers/wkhtmltopdf/index.js
+++ src/helpers/wkhtmltopdf/index.js
@@ -1,4 +1,5 @@
 const wkhtmltopdf = require('wkhtmltopdf')
 
-wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
+//wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
+wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
 module.exports = wkhtmltopdf
