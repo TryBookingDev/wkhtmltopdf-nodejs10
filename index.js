82a7e234b4a0f2b07be9f59e91c44b70b5574526
Michael Magsuci <michael@bookwana.com>
2020-04-24 18:07:29 +0800

    update script

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'
 
 exports.handler = function(event, context) {
     var memStream = new MemoryStream();
-    var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
-    wkhtmltopdf(html_utf8, event.options, function(code, signal) { context.done(null, { pdf_base64: memStream.read().toString('base64') }); }).pipe(memStream);
+    var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
+    wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
+            console.log('doe, code, signal');
+            context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
+    }).pipe(memStream);
 };
