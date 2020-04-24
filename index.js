e52158eeded4b58dc7b706b20589da07735c90fe
Michael Magsuci <michael@bookwana.com>
2020-04-24 21:42:51 +0800

    update script

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.handler = function(event, context) {
     var memStream = new MemoryStream();
     var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
     wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
-            console.log('done, code, signal');
+            console.log('done', code, signal);
             context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
     }).pipe(memStream);
 };
