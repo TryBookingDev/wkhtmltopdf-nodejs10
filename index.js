9cefe832b7b5b0ef6cf57c7f75e593b6e34f6198
Michael Magsuci <michael@bookwana.com>
2020-04-24 17:47:53 +0800

    update script

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var wkhtmltopdf = require('./src/helpers/wkhtmltopdf');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 exports.handler = async (event) => {
+    var writePdfDone = false;
     const html = '<h1>Test</h1>';
     wkhtmltopdf(html, { pageSize: 'letter'}, function(error, stream) {
         console.log("error:", error);
         console.log("Here is the Stream", stream);
+        writePdfDone = true;
     })
 
+    while(writePdfDone === false){
+        
+    }
+    
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
