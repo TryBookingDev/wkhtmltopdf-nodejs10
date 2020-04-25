b940881f6f9126d51fab101da2051aa04728b8fe
Michael Magsuci <michael@bookwana.com>
2020-04-25 11:22:05 +0800

    update scripts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var MemoryStream = require('memorystream');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
+
 exports.handler = function(event, context) {
     var memStream = new MemoryStream();
     var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
--- src/helpers/wkhtmltopdf/index.js
+++ src/helpers/wkhtmltopdf/index.js
@@ -2,5 +2,6 @@ const wkhtmltopdf = require('../../wkhtmltopdf1')
 
 //wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
 //wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
-wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
+process.env.FONTCONFIG_PATH=`${process.cwd()}/fonts`;
+wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`;
 module.exports = wkhtmltopdf
