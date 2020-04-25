2f635c76431140cafedea868ca198290c0f46595
Michael Magsuci <michael@bookwana.com>
2020-04-25 11:50:38 +0800

    test script

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ var MemoryStream = require('memorystream');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
 
-exports.handler = function(event, context) {
+// exports.handler = function(event, context) {
+//     var memStream = new MemoryStream();
+//     var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
+//     console.log('html_utf8', html_utf8);
+//     event.options = event.options || {};
+//     event.options.debug = true;
+//     wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
+//             console.log('done', code, signal);
+//             context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
+//     }).pipe(memStream);
+// };
+
+
+    const html = '<h1>Test</h1>'
+    var event = { html_utf8:html }
     var memStream = new MemoryStream();
-    var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
+    var html_utf8 = event.html_utf8;
     console.log('html_utf8', html_utf8);
     event.options = event.options || {};
     event.options.debug = true;
-    wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
-            console.log('done', code, signal);
-            context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
+    wkhtmltopdf(html_utf8, event.options, function(code, signal) {
+        console.log('done', code, signal);
+        console.log('pdf_base64:', memStream.read().toString('base64'));
     }).pipe(memStream);
-};
 
 
 
