a79d9f333aa8de3aff4b3907445fdfe90c2c4e3c
Michael Magsuci <michael@bookwana.com>
2020-04-25 12:30:49 +0800

    update script

--- src/helpers/wkhtmltopdf/index.js
+++ src/helpers/wkhtmltopdf/index.js
@@ -1,7 +1,10 @@
 const wkhtmltopdf = require('../../wkhtmltopdf1')
 
-//wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
-//wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
 process.env.FONTCONFIG_PATH=`${process.cwd()}/fonts`;
-wkhtmltopdf.command = `${process.cwd()}/wkhtmltopdf`;
+
+//wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
+
+wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
+
+//wkhtmltopdf.command = `${process.cwd()}/wkhtmltopdf`; //working but pdf output has unreadable content
 module.exports = wkhtmltopdf
