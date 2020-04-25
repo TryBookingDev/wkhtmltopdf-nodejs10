ff1d5f77a36afd44493667dcc7241515124309f1
Michael Magsuci <michael@bookwana.com>
2020-04-25 11:34:22 +0800

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 rm -rf deploy
 mkdir deploy
-cp -R wktools src index.js package.json deploy
+cp wktools/bin/wkhtmltopdf deploy
+cp -R src index.js package.json deploy
 cp -r dependencies deploy
 cd deploy
 npm install --production
