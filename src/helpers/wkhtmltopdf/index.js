84f285c57c8d4864b55bb5fd56ea6c0a67670c90
Michael Magsuci <michael@bookwana.com>
2020-04-25 09:17:29 +0800

    update script

--- src/helpers/wkhtmltopdf/index.js
+++ src/helpers/wkhtmltopdf/index.js
@@ -1,5 +1,6 @@
 const wkhtmltopdf = require('wkhtmltopdf')
 
 //wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
-wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
+//wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
+wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
 module.exports = wkhtmltopdf
