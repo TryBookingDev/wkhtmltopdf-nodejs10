c7efd076158938f8e8409e4d8fab742d8a84cd1f
Michael Magsuci <michael@bookwana.com>
2020-04-25 09:38:31 +0800

    update scripts

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ var wkhtmltopdf = require('./src/helpers/wkhtmltopdf');
 //var MemoryStream = require('memorystream');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
-//
-// exports.handler = function(event, context) {
-//     var memStream = new MemoryStream();
-//     var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
-//     wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
-//             console.log('done', code, signal);
-//             context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
-//     }).pipe(memStream);
-// };
-//
-//
 
-exports.handler = async (event) => {
-    const html = '<h1>Test</h1>'
-    wkhtmltopdf(html, { pageSize: 'letter'}, function(error, stream) {
-        console.log("error", error);
-        console.log("Here is the Stream", stream);
-        context.done(null, { pdf_base64: stream.read().toString('base64')});
-    })
+exports.handler = function(event, context) {
+    var memStream = new MemoryStream();
+    var html_utf8 = Buffer.from(event.html_base64, 'base64').toString('utf8');
+    console.log('html_utf8', html_utf8);
+    event.debug = true;
+    wkhtmltopdf(html_utf8, event.options, function(code, signal) { 
+            console.log('done', code, signal);
+            context.done(null, { pdf_base64: memStream.read().toString('base64')}); 
+    }).pipe(memStream);
 };
+
+
+
+// exports.handler = async (event, context) => {
+//     const html = '<h1>Test</h1>'
+//     wkhtmltopdf(html, { pageSize: 'letter'}, function(error, stream) {
+//         console.log("error", error);
+//         console.log("Here is the Stream", stream);
+//         context.done(null, { pdf_base64: stream.read().toString('base64')});
+//     });
+// };
--- src/helpers/wkhtmltopdf/index.js
+++ src/helpers/wkhtmltopdf/index.js
@@ -1,4 +1,4 @@
-const wkhtmltopdf = require('wkhtmltopdf')
+const wkhtmltopdf = require('../../wkhtmltopdf1')
 
 //wkhtmltopdf.command = `${process.cwd()}/wktools/bin/wkhtmltopdf`
 //wkhtmltopdf.command = '/opt/wkhtmltopdf'; //using lambda layer. see https://tech.mybuilder.com/compiling-wkhtmltopdf-aws-lambda-with-bref-easier-than-you-think/
